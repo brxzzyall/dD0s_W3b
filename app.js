60b844075e49b4e5bdba83aa56c551cb7be7b214
Brex666 <127381583+brxzzyall@users.noreply.github.com>
2025-10-29 05:12:08 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-function startAttack() {
+document.getElementById('attackForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const targetURL = document.getElementById('targetURL').value;
+    const attackType = document.getElementById('attackType').value;
+
     fetch('http://<command_server_ip>:3000/start', {
-        method: 'POST'
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ targetURL, attackType })
     }).then(response => response.text())
       .then(data => {
         alert(data);
       });
-}
+});
 
 function stopAttack() {
     fetch('http://<command_server_ip>:3000/stop', {
